fea772ec3ab2c3bf436be517c35f6f959e2a2a50
alx-vieira <alxsv@hotmail.com>
2022-07-20 14:18:18 -0300

    Initial messages(welcome, join room and left chat)

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Connecting
 io.on('connection', (socket) => {
     console.log("New connection detected...");
-    
+
+    // Welcome current user
+    socket.emit('message', 'Welcome to chat!');
+
+    // Broadcast when a user connects for everybody except for the connected user
+    socket.broadcast.emit('message', 'A user has joinned the chat');
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        // for everyone
+        io.emit('message', 'A user has left the chat');
+    });
+
 });
 
 const PORT = 3000 || process.env.PORT;
