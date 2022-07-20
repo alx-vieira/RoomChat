2197b18f71866d0edc64285d783c7a1160d5c909
alx-vieira <alxsv@hotmail.com>
2022-07-20 12:47:18 -0300

    Client page connected

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const path = require('path');
 const express = require('express');
 const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Connecting
+io.on('connection', (socket) => {
+    console.log("New connection detected...");
+    
+});
 
 const PORT = 3000 || process.env.PORT;
 
